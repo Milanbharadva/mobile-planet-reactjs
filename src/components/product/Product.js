96b1d90715023d826da909abb824b839841732f2
Milanbharadva <96285463+Milanbharadva@users.noreply.github.com>
2023-10-20 18:01:25 +0530

    after product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,66 @@
-import Shop from "../shop/Shop";
-
+import { useFetch } from "../../hook/usefetch";
 const Product = () => {
-    return ( <div>
-        <Shop/>
-    </div> );
-}
- 
+  const { data, error, isPending } = useFetch(
+    "https://ecommerce-project-d04f8-default-rtdb.firebaseio.com/product.json"
+  );
+  if (data != null) {
+    var val = Object.values(data);
+  }
+  return (
+    <div className="mt-10 md:mx-20 sm:mx-10 mx-3 lg:mx-20 xl:mx-48 ">
+      {error && (
+        <div className="flex items-center justify-center">
+          <h1 className="text-3xl font-semibold text-red-700">
+            Error in fetch please try again later.
+          </h1>
+        </div>
+      )}
+      {isPending && (
+        <div className="flex items-center mt-2 justify-center">
+          <h1 className="text-2xl font-semibold ">Loading...</h1>
+        </div>
+      )}
+      <div >
+        {val &&
+          val.map((item) => (
+            <div
+              key={item.ID}
+              className="flex justify-around py-5 border-2 items-center md:items-start md:flex-row flex-col gap-8"
+            >
+              <div className="lg:w-[20%] object-contain">
+                <img
+                  src={`${window.location.origin}/assets/product/${item.productimage}`}
+                  height="200px"
+                  width="200px"
+                  alt={item.productname}
+                />
+              </div>
+              <div className="flex flex-col justify-around md:w-[30%] lg:w-[50%] gap-4 px-3">
+                <h2 className="text-xl font-bold">{`${item.productname.toUpperCase()} ( ${
+                  item.productcolor
+                } , ${item.productrom}GB )`}</h2>
+                <ul className="flex flex-col gap-1">
+                  <li className="">{`${item.productram} GB RAM | ${item.productrom} GB ROM`}</li>
+                  <li className="">{item.productdisplay}</li>
+                  <li className="">{item.productcamera}</li>
+                  <li className="">{item.productbattery}</li>
+                  <li className="">{item.productprocessor}</li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-3  items-center">
+                <h2 className="text-xl font-bold">{item.productprice}</h2>
+                <button className="text-white bg-[#F28123] h-[50px] w-[200px] rounded-[50px]">
+                  Details
+                </button>
+                <button className="text-white bg-[#F28123] h-[50px] w-[200px] rounded-[50px]">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
 export default Product;
